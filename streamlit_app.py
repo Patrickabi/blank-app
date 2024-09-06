c247c26b868c8311d2a7b35f8efaf132a333d0f6
Patrickabi <patrick.abi@hotmail.com>
2024-09-06 16:58:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import plotly.graph_objects as go
 from patrick5day_rfmodel_068 import train_rf_model, plot_feature_importance
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import OneHotEncoder
