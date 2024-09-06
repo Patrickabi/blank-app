c01bf7caab0f83ce923e51054a2cd2fd1f6d4bbb
Patrickabi <patrick.abi@hotmail.com>
2024-09-06 16:57:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from patrick5day_rfmodel_068 import train_rf_model, plot_feature_importance
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+import plotly.express as px
+import plotly.graph_objects as go
 
 def plot_feature_influence(features, importances):
     plt.figure(figsize=(30, 18))
