4d1738022ba094ddb62f6d3347f3dc38f05f1d4d
Patrickabi <patrick.abi@hotmail.com>
2024-09-07 19:40:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,4 +57,43 @@ elif option == "Shampoo":
     with st.spinner('Wait for it...'):
         time.sleep(5)
         st.success("Done!")
+
+elif option == "Deodorants":
+    df = pd.read_csv("Base_Baby_18kv3.csv")
+    # Create ipyvizzu Object with the DataFrame
+    obj = create_vizzu_obj(df)
     
+    # Preset plot usage. Preset plots works directly with DataFrames.
+    bar_obj = bar_chart(df,
+                x = "Kinds", 
+                y = "Popularity",
+                title= "1.Using preset plot function `bar_chart()`"
+                )
+    
+    # Animate with defined arguments 
+    anim_obj = beta_vizzu_animate( bar_obj,
+        x = "Genres",
+        y =  ["Popularity", "Kinds"],
+        title = "Animate with beta_vizzu_animate () function",
+        label= "Popularity",
+        color="Genres",
+        legend="color",
+        sort="byValue",
+        reverse=True,
+        align="center",
+        split=False,
+    )
+    
+    # Animate with general dict based arguments 
+    _dict = {"size": {"set": "Popularity"}, 
+        "geometry": "circle",
+        "coordSystem": "polar",
+        "title": "Animate with vizzu_animate () function",
+        }
+    anim_obj2 = vizzu_animate(anim_obj,_dict)
+    
+    # Visualize within Streamlit
+    with st.container(): # Maintaining the aspect ratio
+        st.button("Animate")
+        vizzu_plot(anim_obj2)
+        
