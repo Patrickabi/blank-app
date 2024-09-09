2166b806ceeb91e416531d50afb20bd3d83dc95b
Patrickabi <patrick.abi@hotmail.com>
2024-09-09 06:33:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ if option == "Diapers":
     my_bar = st.progress(0, text=progress_text)
     
     for percent_complete in range(100):
-        time.sleep(0.01)
-        my_bar.progress(percent_complete + 2, text=progress_text)
+        time.sleep(0.005)
+        my_bar.progress(percent_complete + 1, text=progress_text)
     time.sleep(1)
     my_bar.empty()
     
