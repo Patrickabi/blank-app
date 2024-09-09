4b9500cac7f9e9dfb35dec86ff3165d9fd901905
Patrickabi <patrick.abi@hotmail.com>
2024-09-09 06:31:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,26 +31,34 @@ option = st.selectbox(
 
 if option == "Diapers":
 
-
+    rf_model, X_train = train_rf_model()
     progress_text = "Operation in progress. Please wait."
     my_bar = st.progress(0, text=progress_text)
     
     for percent_complete in range(100):
-        time.sleep(0.01)
+        time.sleep(0.05)
         my_bar.progress(percent_complete + 1, text=progress_text)
     time.sleep(1)
     my_bar.empty()
+    
+    
+    st.write ("""In our review of the key factors influencing diaper purchase decisions, we conducted an in-depth analysis of customer preferences.
+    By examining various features, such as brand, product volume, material quality, and others, we identified the major driving forces behind consumer choices.
+    Our findings revealed that Brand, Volume, and Material stand out as the most influential factors.
+    These elements play a pivotal role in shaping customer decisions, highlighting the significance of brand loyalty, product capacity, and material composition in the diaper selection process.""")
 
+    
 
     st.title('Random Forest Feature Importance')
-    # Train the model and get feature importance
-    rf_model, X_train = train_rf_model()
-    
-    # Plot the feature importance using Plotly
+
+    # Plot the feature importance 
     fig = plot_feature_importance(rf_model, X_train)
-    
-    # Display the Plotly figure in Streamlit
-    st.plotly_chart(fig)
+
+    on = st.toggle("Show Graph")
+
+    if on:
+        st.plotly_chart(fig)
+
 elif option == "Shampoo":
     with st.spinner('Wait for it...'):
         time.sleep(5)
