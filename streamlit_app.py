2c6d479876d041954e4aa7ee7dd3398944ceeae2
Patrickabi <patrick.abi@hotmail.com>
2024-09-10 09:48:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,9 @@ from st_vizzu import *
 import visgraph
 from shampoo_model import *
 from deodorant_model import get_deodo_data, deodo_model, plot_deodo_feature_importance
+from stream import logo
+
+logo()
 
 st.logo(
     LOGO_URL_LARGE,
