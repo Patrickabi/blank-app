16341d11cfad75d5038a5aa3cf618de797ef6d87
Patrickabi <patrick.abi@hotmail.com>
2024-09-10 18:23:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-from diaper_model import train_rf_model, plot_feature_importance, create_df, plot_feature_importance1
+from diaper_model import train_rf_model, plot_feature_importance, create_df
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
