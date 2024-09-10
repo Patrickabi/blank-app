8d5f212473554905f64d2f9972522acf4789e058
Patrickabi <patrick.abi@hotmail.com>
2024-09-10 07:46:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-from patrick5day_rfmodel_068 import train_rf_model, plot_feature_importance, create_df
+from diaper_model import train_rf_model, plot_feature_importance, create_df
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
